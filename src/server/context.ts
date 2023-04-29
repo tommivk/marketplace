065af8ae5af9b39683fff225b78db707f2e70dd6
tommivk <tommikokk@gmail.com>
2023-04-29 15:39:31 +0300

    add userId to context

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,11 +1,18 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { PrismaClient } from "@prisma/client";
+import { getAuth } from "@clerk/nextjs/server";
 
-export async function createContext(opts?: trpcNext.CreateNextContextOptions) {
+export async function createContext(opts: trpcNext.CreateNextContextOptions) {
+  const { req } = opts;
+  const { userId } = getAuth(req);
+
   const prisma = new PrismaClient();
 
-  return { prisma };
+  return {
+    prisma,
+    userId,
+  };
 }
 
 export type Context = trpc.inferAsyncReturnType<typeof createContext>;
