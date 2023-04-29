388d94fa1b7b4bdd3faaf6b34424032c323eb038
tommivk <tommikokk@gmail.com>
2023-04-29 15:59:23 +0300

    add items router

--- src/server/root.ts
+++ src/server/root.ts
@@ -1,8 +1,10 @@
 import { router } from "./trpc";
 import { usersRouter } from "./routers/users";
+import { itemsRouter } from "./routers/items";
 
 export const appRouter = router({
   users: usersRouter,
+  items: itemsRouter,
 });
 
 export type AppRouter = typeof appRouter;
---
+++ src/server/routers/items.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+import { procedure, protectedProcedure, router } from "../trpc";
+import { TRPCError } from "@trpc/server";
+
+export const itemsRouter = router({
+  getAll: procedure.query(async ({ ctx }) => {
+    const items = await ctx.prisma.item.findMany();
+    return items;
+  }),
+
+  findById: procedure
+    .input(z.object({ itemId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const item = await ctx.prisma.item.findUnique({
+        where: { id: input.itemId },
+      });
+      if (!item) throw new TRPCError({ code: "NOT_FOUND" });
+
+      return item;
+    }),
+
+  create: protectedProcedure
+    .input(
+      z.object({
+        title: z.string().min(2).max(50),
+        description: z.string().min(2).max(2000),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const item = await ctx.prisma.item.create({
+        data: {
+          ...input,
+          authorId: ctx.userId,
+        },
+      });
+
+      return item;
+    }),
+});
