b07fc879a96aa62876e2addadf92c656349c3548
tommivk <tommikokk@gmail.com>
2023-04-29 15:39:00 +0300

    add clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import "@/styles/globals.css";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps} appearance={{ baseTheme: dark }}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
