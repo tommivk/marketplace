9402136bf38227dcc9b7009b667876a0a3f1cd78
tommivk <tommikokk@gmail.com>
2023-04-29 15:40:14 +0300

    add middleware

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,7 +1,20 @@
-import { initTRPC } from "@trpc/server";
+import { TRPCError, initTRPC } from "@trpc/server";
 import { Context } from "./context";
 
 const t = initTRPC.context<Context>().create();
 
 export const router = t.router;
 export const procedure = t.procedure;
+
+const checkAuth = t.middleware(async ({ ctx, next }) => {
+  const { userId } = ctx;
+  if (!userId) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+  return next({
+    ctx: {
+      userId,
+    },
+  });
+});
+
+export const protectedProcedure = t.procedure.use(checkAuth);
