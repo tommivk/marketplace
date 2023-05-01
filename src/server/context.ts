3b8ca489ff8139549f6bee0655f6e26174f2539d
tommivk <tommikokk@gmail.com>
2023-05-01 14:50:18 +0300

    save prisma instance to global object

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,13 +1,11 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "./db";
 import { getAuth } from "@clerk/nextjs/server";
 
 export async function createContext(opts: trpcNext.CreateNextContextOptions) {
   const { req } = opts;
   const { userId } = getAuth(req);
-
-  const prisma = new PrismaClient();
 
   return {
     prisma,
