e598ea93f125ab061208931ed5e1f6838070a1a1
tommivk <tommikokk@gmail.com>
2023-05-01 23:44:17 +0300

    set superjson as data transformer

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,7 +1,8 @@
 import { TRPCError, initTRPC } from "@trpc/server";
 import { Context } from "./context";
+import superjson from "superjson";
 
-const t = initTRPC.context<Context>().create();
+const t = initTRPC.context<Context>().create({ transformer: superjson });
 
 export const router = t.router;
 export const procedure = t.procedure;
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,6 +1,8 @@
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import type { AppRouter } from "../server/root";
+import superjson from "superjson";
+
 function getBaseUrl() {
   if (typeof window !== "undefined")
     // browser should use relative path
@@ -17,6 +19,7 @@ function getBaseUrl() {
 export const trpc = createTRPCNext<AppRouter>({
   config(opts) {
     return {
+      transformer: superjson,
       links: [
         httpBatchLink({
           /**
