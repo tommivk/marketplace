92f9342538082567c9fce3b00b2edcc1ce673279
tommivk <tommikokk@gmail.com>
2023-05-01 14:39:17 +0300

    add categories model

--- prisma/migrations/20230429101237_marketdb/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Item" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "authorId" TEXT NOT NULL,
-
-    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
-);
