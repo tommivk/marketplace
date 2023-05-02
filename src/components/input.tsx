3045bea2aa7cd57e9dfb02c96886e5329d1cf896
tommivk <tommikokk@gmail.com>
2023-05-02 19:57:17 +0300

    add input component

---
+++ src/components/input.tsx
@@ -0,0 +1,24 @@
+import { forwardRef } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
+
+type Props = {
+  ref: UseFormRegisterReturn<string>;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+// eslint-disable-next-line react/display-name
+const Input = forwardRef<HTMLInputElement, Props>(({ ...props }, ref) => {
+  return (
+    <input
+      {...props}
+      className={`py-2 px-3 w-full text-sm 
+           bg-slate-200 bg-opacity-5 
+           rounded-md outline-none 
+           text-slate-200
+           ${props.className}
+          `}
+      ref={ref}
+    />
+  );
+});
+
+export default Input;
