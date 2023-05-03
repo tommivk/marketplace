f59d14999c5e7cd6869cec35f96b6ecc1aa3a992
tommivk <tommikokk@gmail.com>
2023-05-03 14:51:08 +0300

    update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,13 +5,19 @@ const prisma = new PrismaClient();
 // run with: npx prisma db seed
 
 async function main() {
-  const categories = ["Cars", "Guitars", "Potatoes", "Other"];
+  const categories = [
+    { name: "Cars", imageURL: "/category/car.jpg" },
+    { name: "Guitars", imageURL: "/category/guitar.jpg" },
+    { name: "Potatoes", imageURL: "/category/potato.jpg" },
+    { name: "Bikes", imageURL: "/category/bike.jpg" },
+    { name: "Other", imageURL: "/category/other.jpg" },
+  ];
 
-  categories.map(async (name) => {
+  for (let category of categories) {
     await prisma.category.create({
-      data: { name },
+      data: { ...category },
     });
-  });
+  }
 }
 
 main()
