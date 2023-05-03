02451053662727ecb75fb2283eac82e7aa92328e
tommivk <tommikokk@gmail.com>
2023-05-03 15:07:02 +0300

    add CategoryCard component

---
+++ src/components/CategoryCard.tsx
@@ -0,0 +1,29 @@
+import { AppRouter } from "@/server/root";
+import { inferRouterOutputs } from "@trpc/server";
+import ImageCard from "./ImageCard";
+import { trpc } from "@/utils/trpc";
+
+type RouterOutput = inferRouterOutputs<AppRouter>;
+type Category = RouterOutput["categories"]["getAll"][number];
+
+type Props = {
+  category: Category;
+};
+
+const CategoryCard = ({ category }: Props) => {
+  const { data: itemCount, isLoading: itemCountLoading } =
+    trpc.categories.getItemCount.useQuery({
+      categoryId: category.id,
+    });
+
+  return (
+    <ImageCard
+      title={category.name}
+      imageURL={category.imageURL}
+      content={itemCountLoading ? "" : `${itemCount} Items`}
+      link={""}
+    />
+  );
+};
+
+export default CategoryCard;
