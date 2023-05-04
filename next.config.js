f44bda4650dd1c0ebd4911b324a1ee79f189affc
tommivk <tommikokk@gmail.com>
2023-05-04 16:42:54 +0300

    add image remote patterns

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tommivk-marketplace.imgix.net',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  }
 }
 
 module.exports = nextConfig
