41dfc07ec9fb114668c690eb0cb48d7d32cd77b7
tommivk <tommikokk@gmail.com>
2023-05-09 22:01:10 +0300

    create zustand store

---
+++ src/store/useFormStore.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+import { contactDetailsSchema, itemSchemaWithFile } from "../schema";
+import { z } from "zod";
+
+type ItemDetailsType = z.infer<typeof itemSchemaWithFile>;
+type ContactDetailsType = z.infer<typeof contactDetailsSchema>;
+
+type SetDataType =
+  | { step: "itemDetails"; data: ItemDetailsType }
+  | { step: "contactDetails"; data: ContactDetailsType };
+
+export const useFormStore = create<{
+  itemDetails: ItemDetailsType | undefined;
+  contactDetails: ContactDetailsType | undefined;
+  setData: ({ step, data }: SetDataType) => void;
+}>((set) => ({
+  itemDetails: undefined,
+  contactDetails: undefined,
+  setData: ({ step, data }) =>
+    set((state) => ({
+      ...state,
+      [step]: data,
+    })),
+}));
