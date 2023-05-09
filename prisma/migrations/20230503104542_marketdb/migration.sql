642be3c04ed00316422244cbba990c9e00e46a90
tommivk <tommikokk@gmail.com>
2023-05-09 15:57:11 +0300

    switch to mySQL

--- prisma/migrations/20230503104542_marketdb/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `imageURL` to the `Category` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Category" ADD COLUMN     "imageURL" TEXT NOT NULL;
