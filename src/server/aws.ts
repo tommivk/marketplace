6faa0a4f30a3f68dc7d10e27f5f91fc78a89450e
tommivk <tommikokk@gmail.com>
2023-05-09 16:32:02 +0300

    update env variable name

--- src/server/aws.ts
+++ src/server/aws.ts
@@ -9,7 +9,7 @@ import { v4 as uuidv4 } from "uuid";
 const s3 = new S3Client({
   region: "eu-north-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY ?? "",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "",
   },
 });
