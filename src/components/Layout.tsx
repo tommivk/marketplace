5aa148bfdf146938e34add71969adc64daaddf54
tommivk <tommikokk@gmail.com>
2023-05-10 16:34:09 +0300

    add redirect to finalize page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,37 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect, useMemo } from "react";
 import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
 import Link from "next/link";
 import Button from "./Button";
 import { useRouter } from "next/router";
 
 export default function Layout({ children }: PropsWithChildren) {
+  const { user } = useClerk();
+  const router = useRouter();
+
+  const redirectToFinalize = useMemo(
+    () =>
+      router.pathname !== "/finalize-user" &&
+      !!user &&
+      !user.publicMetadata.finalized,
+    [user, router]
+  );
+
+  useEffect(() => {
+    if (redirectToFinalize) {
+      router.replace("/finalize-user");
+    }
+  }, [redirectToFinalize, router]);
+
+  const isLoading = user === undefined;
+
   return (
     <div className="flex flex-col h-full">
       <Navigation />
-      <main className="flex-1">{children}</main>
+      {isLoading || redirectToFinalize ? (
+        <>...Loading</>
+      ) : (
+        <main className="flex-1">{children}</main>
+      )}
     </div>
   );
 }
@@ -17,9 +40,9 @@ const Navigation = () => {
   const { signOut } = useClerk();
   const router = useRouter();
 
-  const handleSignout = (e: React.MouseEvent<HTMLElement>) => {
+  const handleSignout = async (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
-    signOut();
+    await signOut();
     router.push("/");
   };
 
