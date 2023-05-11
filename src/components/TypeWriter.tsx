aa17829b49eb1770d374a70ce4ad8b2c7979e7d9
tommivk <tommikokk@gmail.com>
2023-05-11 21:49:10 +0300

    add typewriter effect

---
+++ src/components/TypeWriter.tsx
@@ -0,0 +1,33 @@
+import { useState, useCallback, useEffect } from "react";
+
+type Props = {
+  words: string[];
+  className?: string;
+};
+
+const TypeWriter = ({ words, className }: Props) => {
+  const [index, setIndex] = useState(0);
+
+  const handleUpdate = useCallback(() => {
+    if (index + 1 < words.length) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }, [words, index]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleUpdate();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [handleUpdate]);
+
+  return (
+    <h1 key={index} className={`typewriter ${className}`}>
+      {words[index]}
+    </h1>
+  );
+};
+
+export default TypeWriter;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import CategoryCard from "@/components/CategoryCard";
 import { trpc } from "@/utils/trpc";
 import { useRouter } from "next/router";
@@ -9,6 +9,7 @@ import { prisma } from "@/server/db";
 import superjson from "superjson";
 import Carousel from "@/components/Carousel";
 import ImageCard from "@/components/ImageCard";
+import TypeWriter from "@/components/TypeWriter";
 
 export default function Home() {
   const { data: newestItems } = trpc.items.getNewest.useQuery();
@@ -32,15 +33,25 @@ export default function Home() {
     <CategoryCard category={category} key={category.id} />
   ));
 
+  const words = useMemo(
+    () =>
+      categories?.map((c) =>
+        c.name == "Other" ? "Anything..." : `${c.name}...`
+      ) ?? [],
+    [categories]
+  );
+
   return (
     <div className="flex flex-col items-center mb-20">
       <div className="mt-48 mb-48 max-w-full w-[500px] px-2">
-        <h1 className="text-4xl text-center font-extrabold">
-          Search For{" "}
-          <span className="text-transparent text-4xl bg-clip-text bg-gradient-to-r from-fuchsia-700 to-blue-400">
-            ANYTHING
-          </span>
-        </h1>
+        <div className="relative flex flex-col sm:flex-row gap-3 flex-wrap sm:ml-14">
+          <h1 className="text-4xl text-center font-extrabold">Search For </h1>
+          <TypeWriter
+            words={words}
+            className="text-4xl text-center font-extrabold m-auto sm:ml-0 bg-gradient-to-r from-fuchsia-700 to-blue-400"
+          />
+        </div>
+
         <input
           type="text"
           className="px-4 py-2 w-full rounded-lg mt-20 text-black"
