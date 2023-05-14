2e6466f8d76f089ca062f34069e1ba3d7fff38ef
tommivk <tommikokk@gmail.com>
2023-05-14 23:54:11 +0300

    add loader

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 const BASE_URL = process.env.IMGIX_BASE_URL;
 
 const NotFound = () => {
+  const loader = ({ src }: { src: string }) => {
+    return src;
+  };
   return (
     <div className="text-gray-400 select-none">
-      <Image src={`${BASE_URL}/404.gif`} alt="Not found" fill />
+      <Image src={`${BASE_URL}/404.gif`} alt="Not found" fill loader={loader} />
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center">
         <h1 className="text-[100px] md:text-[350px] font-semibold leading-none">
           404
