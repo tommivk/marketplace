e434268774f6f9a6fd8b850ca76ced1dd72fc787
tommivk <tommikokk@gmail.com>
2023-05-14 19:37:12 +0300

    add class name prop

--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -1,6 +1,9 @@
-const FormContainer = ({ children }: React.PropsWithChildren) => {
+const FormContainer = ({
+  children,
+  className,
+}: React.PropsWithChildren & { className?: string }) => {
   return (
-    <div className="h-full flex justify-center items-center">
+    <div className={`h-full flex justify-center items-center ${className}`}>
       <div className="bg-zinc-900 px-6 py-6 w-[500px] m-5 rounded-lg">
         {children}
       </div>
