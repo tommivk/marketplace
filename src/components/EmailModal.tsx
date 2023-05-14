9317358e2eeda345cd5c1a82144a6504ddd6d3a2
tommivk <tommikokk@gmail.com>
2023-05-14 20:07:28 +0300

    check if user is logged in

--- src/components/EmailModal.tsx
+++ src/components/EmailModal.tsx
@@ -5,6 +5,8 @@ import Button from "./Button";
 import FormContainer from "./FormContainer";
 import FormField from "./FormField";
 import FormLabel from "./FormLabel";
+import { useRouter } from "next/router";
+import { useClerk } from "@clerk/nextjs";
 
 const EmailModal = ({
   itemId,
@@ -15,6 +17,9 @@ const EmailModal = ({
   modalOpen: boolean;
   setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const { session } = useClerk();
+  const router = useRouter();
+
   const messageRef = useRef<HTMLTextAreaElement>(null);
 
   const { mutate, isLoading } = trpc.emails.sendEmail.useMutation({
@@ -48,27 +53,42 @@ const EmailModal = ({
         >
           X
         </div>
-        <FormField>
-          <h1 className="text-2xl text-center mb-4">Send Email</h1>
-          <FormLabel text="Message" />
+        {session ? (
+          <>
+            <FormField>
+              <h1 className="text-2xl text-center mb-4">Send Email</h1>
+              <FormLabel text="Message" />
 
-          <textarea
-            ref={messageRef}
-            className="w-full h-64 bg-zinc-800 text-slate-200 py-2 px-4"
-          />
-          <p className="text-xs text-zinc-700 p-2">
-            (Email will be sent to a dummy AWS address)
-          </p>
-        </FormField>
-        <FormField>
-          <Button
-            className="m-auto block mt-6"
-            onClick={handleSubmit}
-            loading={isLoading}
-          >
-            Send Email
-          </Button>
-        </FormField>
+              <textarea
+                ref={messageRef}
+                className="w-full h-64 bg-zinc-800 text-slate-200 py-2 px-4"
+              />
+              <p className="text-xs text-zinc-700 p-2">
+                (Email will be sent to a dummy AWS address)
+              </p>
+            </FormField>
+            <FormField>
+              <Button
+                className="m-auto block mt-6"
+                onClick={handleSubmit}
+                loading={isLoading}
+              >
+                Send Email
+              </Button>
+            </FormField>
+          </>
+        ) : (
+          <div className="flex flex-col justify-center items-center gap-10 px-10 py-20 box-border">
+            <h3 className="text-xl">You must be logged in to send emails</h3>
+            <Button
+              onClick={() => {
+                router.push(`/login?r=items/${itemId}`);
+              }}
+            >
+              Login
+            </Button>
+          </div>
+        )}
       </FormContainer>
     </div>
   );
