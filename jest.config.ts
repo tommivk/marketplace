ee8d04c7a14b5f95005f7952bf5be566bbdaad7c
tommivk <tommikokk@gmail.com>
2023-05-16 02:25:43 +0300

    add jest config

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  clearMocks: true,
+  coverageProvider: "v8",
+  preset: "ts-jest/presets/js-with-ts",
+  setupFiles: ["dotenv/config"],
+  transform: {
+    "^.+\\.mjs$": "ts-jest",
+  },
+};
+
+export default config;
