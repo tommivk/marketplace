3546c6fd30245a8be70cdbba1920211dbee4ce1a
tommivk <tommikokk@gmail.com>
2023-06-03 12:03:09 +0300

    add progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,27 +5,31 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { Toaster } from "react-hot-toast";
 import Layout from "@/components/Layout";
+import ProgressBar from "nextjs-progressbar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps} appearance={{ baseTheme: dark }}>
-      <Layout>
-        <Component {...pageProps} />
-        <Toaster
-          position="bottom-right"
-          toastOptions={{
-            success: {
-              style: {
-                background: "#343434",
-                color: "whitesmoke",
-                fontSize: "15px",
+    <>
+      <ProgressBar height={1} options={{ showSpinner: false }} />
+      <ClerkProvider {...pageProps} appearance={{ baseTheme: dark }}>
+        <Layout>
+          <Component {...pageProps} />
+          <Toaster
+            position="bottom-right"
+            toastOptions={{
+              success: {
+                style: {
+                  background: "#343434",
+                  color: "whitesmoke",
+                  fontSize: "15px",
+                },
+                duration: 4000,
               },
-              duration: 4000,
-            },
-          }}
-        />
-      </Layout>
-    </ClerkProvider>
+            }}
+          />
+        </Layout>
+      </ClerkProvider>
+    </>
   );
 };
 
