d6df893cb92cdad48a4ffccac2585544d0679b23
tommivk <tommikokk@gmail.com>
2023-06-07 06:39:52 +0300

    add size prop to Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,11 +1,27 @@
-const Loading = ({ className }: { className?: string }) => {
+type Props = {
+  className?: string;
+  size?: "sm" | "lg";
+};
+
+const Loading = ({ className, size = "lg" }: Props) => {
+  const sizes = {
+    sm: "h-2 w-2",
+    lg: "h-5 w-5",
+  };
+
   return (
     <div
       className={`flex h-full items-center justify-center gap-2 ${className}`}
     >
-      <span className="h-5 w-5 animate-loader rounded-full bg-zinc-500"></span>
-      <span className="animation-delay-200 h-5 w-5 animate-loader rounded-full bg-zinc-500"></span>
-      <span className="animation-delay-400 h-5 w-5 animate-loader rounded-full bg-zinc-500 "></span>
+      <span
+        className={`${sizes[size]} animate-loader rounded-full bg-zinc-500`}
+      ></span>
+      <span
+        className={`${sizes[size]} animation-delay-200 animate-loader rounded-full bg-zinc-500`}
+      ></span>
+      <span
+        className={`${sizes[size]} animation-delay-400 animate-loader rounded-full bg-zinc-500`}
+      ></span>
     </div>
   );
 };
